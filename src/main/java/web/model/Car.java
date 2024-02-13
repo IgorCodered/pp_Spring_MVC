057a6cba756636ea98b53f7204a30366dd7b2dea
Igor Elesin <149936179+IgorCodered@users.noreply.github.com>
2024-02-13 18:15:51 +0700

    Car POJO

---
+++ src/main/java/web/model/Car.java
@@ -0,0 +1,85 @@
+package web.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table(name = "car")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "model")
+    private String model;
+    @Column(name = "max_speed")
+    private int maxSpeed;
+    @Column(name = "date")
+    private LocalDate date;
+
+    public Car() {
+    }
+
+    public Car(String model, int maxSpeed, LocalDate date) {
+        this.model = model;
+        this.maxSpeed = maxSpeed;
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return maxSpeed == car.maxSpeed && Objects.equals(id, car.id) && Objects.equals(model, car.model) && Objects.equals(date, car.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, model, maxSpeed, date);
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", model='" + model + '\'' +
+                ", maxSpeed=" + maxSpeed +
+                ", date=" + date +
+                '}';
+    }
+}
