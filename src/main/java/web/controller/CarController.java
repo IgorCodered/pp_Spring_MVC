711cd96812e36271220b630c09284d9c5380c3f6
Igor Elesin <149936179+IgorCodered@users.noreply.github.com>
2024-02-15 16:37:20 +0700

    Done

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,24 +1,39 @@
 package web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
-import web.service.CarService;
 
+import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class CarController {
 
-    private final CarService service;
+    List<Car> cars = Arrays.asList(
+            new Car("BMW", 180, "5"),
+            new Car("Лада", 300, "3"),
+            new Car("Мерседес", 789, "1"),
+            new Car("Бугатти", 123, "5"),
+            new Car("Порш", 345, "5"),
+            new Car("Ауди", 250, "3"),
+            new Car("Лада", 200, "2"),
+            new Car("Ока", 1000, "5"));
 
-    public CarController(CarService service) {
-        this.service = service;
-    }
 
     @GetMapping("/cars")
-    public List<Car> getCountCars(@RequestParam("count") int count) {
-        return service.getCountCars(count);
+    public String getCountCars(@RequestParam(value = "count", defaultValue = "0") int count,
+                               ModelMap map) {
+        List<Car> carsCounting;
+        if (count <= 0) {
+            carsCounting = cars;
+        } else {
+            carsCounting = cars.stream().limit(count).toList();
+        }
+        map.addAttribute("cars", carsCounting);
+        return "cars";
     }
 }
--- src/main/java/web/model/Car.java
+++ src/main/java/web/model/Car.java
@@ -1,38 +1,20 @@
 package web.model;
 
-import javax.persistence.*;
 import java.time.LocalDate;
-import java.util.Objects;
 
-@Entity
-@Table
+
 public class Car {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @Column(name = "model")
     private String model;
-    @Column(name = "max_speed")
     private int maxSpeed;
-    @Column(name = "date")
-    private LocalDate date;
+    private String series;
 
     public Car() {
     }
 
-    public Car(String model, int maxSpeed, LocalDate date) {
+    public Car(String model, int maxSpeed, String series) {
         this.model = model;
         this.maxSpeed = maxSpeed;
-        this.date = date;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
+        this.series = series;
     }
 
     public String getModel() {
@@ -51,34 +33,20 @@ public class Car {
         this.maxSpeed = maxSpeed;
     }
 
-    public LocalDate getDate() {
-        return date;
+    public String getSeries() {
+        return series;
     }
 
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Car car = (Car) o;
-        return maxSpeed == car.maxSpeed && Objects.equals(id, car.id) && Objects.equals(model, car.model) && Objects.equals(date, car.date);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, model, maxSpeed, date);
+    public void setSeries(String series) {
+        this.series = series;
     }
 
     @Override
     public String toString() {
         return "Car{" +
-                "id=" + id +
-                ", model='" + model + '\'' +
+                "model='" + model + '\'' +
                 ", maxSpeed=" + maxSpeed +
-                ", date=" + date +
+                ", date=" + series +
                 '}';
     }
 }
